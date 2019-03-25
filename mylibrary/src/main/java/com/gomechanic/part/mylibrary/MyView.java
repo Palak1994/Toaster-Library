5731dce45d8c9942cec01b40392b4456a2c2b626
palak sachdeva <palak@ziffyhomes.com>
2019-03-25 16:41:03 +0530

    initial commt

---
+++ mylibrary/src/main/java/com/gomechanic/part/mylibrary/MyView.java
@@ -0,0 +1,16 @@
+package com.gomechanic.part.mylibrary;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class MyView extends LinearLayout {
+
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ mylibrary/src/main/java/com/gomechanic/part/mylibrary/RssFeedProvider.java
@@ -0,0 +1,18 @@
+package com.gomechanic.part.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class RssFeedProvider {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+
+
+}
